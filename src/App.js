345c2d6b130a6cd56c90a3a9bb2bb0dbe6afe1a2
Dmitry Tsarapkine <dimazonyoutubing@gmail.com>
2019-08-28 14:25:47 -0400

    Work Experience Window Box
    Uploaded so I can finish the rest on my desktop.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <AboutTerminal />
       <OtherExperience />
+      <WorkExperience />
     </div>
   );
 }
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import WindowHeader from './WindowHeader'
+import './WorkExperience.css'
+
 export default class WorkExperience extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return (<div></div>)
+        return (
+        <div className = 'work-experience'>
+            <WindowHeader title = 'Work Experience'/>
+            <div className = 'work-experience-body'>
+                <div className = 'work-position'>
+                    <div className = 'work-icon'>
+                    </div>
+                    <b className = 'company-name'>
+                            Google
+                    </b>
+                    <t className = 'date'>Today</t>
+                </div>
+                <div className = 'work-position'>
+                    <div className = 'work-icon'>
+
+                    </div>
+                </div>
+            </div> 
+
+
+        </div>
+        )
     }
 }
