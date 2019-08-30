142619829b103f53b944e1b8fbe7ac57e0700e00
Dmitry Tsarapkine <32915731+Dimatsa@users.noreply.github.com>
2019-08-30 14:49:32 -0400

    Work Experience Section

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -15,16 +15,106 @@ export default class WorkExperience extends Component {
                 <div className = 'work-position'>
                     <div className = 'work-icon'>
                     </div>
-                    <b className = 'company-name'>
-                            Google
-                    </b>
-                    <t className = 'date'>Today</t>
+                    <b className = 'company-name'>Google</b>
+                    <t className = 'date'>March 2019 - August 2019</t>
+                    <br class = 'new-line'></br>
+                    <t className = 'position-name'>Engineer</t>
+                    <t className = 'location'>Los Angeles, CA</t>
+                    <br class = 'new-line'></br>
+                    <t className = 'work-description'>
+                    Lorem ipsum dolor sit amet, scelerisque vestibulum, faucibus egestas vehicula mi viverra auctor tristique. Pede montes, rhoncus tempus a per, ante et. Et bibendum ridiculus illo praesent in iure, sed ut posuere. Dui a dignissimos aliquam hendrerit consectetuer ipsum, malesuada sit morbi, iaculis non.</t>
+                    <br></br>
+                    <div className = 'skills'>
+                        <div className = 'language'>
+                            <b className = 'language-body'>Python</b>
+                        </div>
+                        <div className = 'technology'>
+                            <b className = 'technology-body'>Node.js</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>JavaScript</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>Python</b>
+                        </div>
+                        <div className = 'technology'>
+                            <b className = 'technology-body'>Node.js</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>JavaScript</b>
+                        </div>
+                    </div>
                 </div>
                 <div className = 'work-position'>
                     <div className = 'work-icon'>
-
+                    </div>
+                    <b className = 'company-name'>Google</b>
+                    <t className = 'date'>Today</t>
+                    <br class = 'new-line'></br>
+                    <t className = 'position-name'>Engineer</t>
+                    <t className = 'location'>Los Angeles, CA</t>
+                    <br class = 'new-line'></br>
+                    <t className = 'work-description'>
+                    Lorem ipsum dolor sit amet, scelerisque vestibulum, faucibus egestas vehicula mi viverra auctor tristique. Pede montes, rhoncus tempus a per, ante et. Et bibendum ridiculus illo praesent in iure, sed ut posuere. Dui a dignissimos aliquam hendrerit consectetuer ipsum, malesuada sit morbi, iaculis non.</t>
+                    <br></br>
+                    <div className = 'skills'>
+                        <div className = 'language'>
+                            <b className = 'language-body'>Python</b>
+                        </div>
+                        <div className = 'technology'>
+                            <b className = 'technology-body'>Node.js</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>JavaScript</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>Python</b>
+                        </div>
+                        <div className = 'technology'>
+                            <b className = 'technology-body'>Node.js</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>JavaScript</b>
+                        </div>
                     </div>
                 </div>
+                <div className = 'work-position'>
+                    <div className = 'work-icon'>
+                    </div>
+                    <b className = 'company-name'>Google</b>
+                    <t className = 'date'>Today</t>
+                    <br class = 'new-line'></br>
+                    <t className = 'position-name'>Engineer</t>
+                    <t className = 'location'>Los Angeles, CA</t>
+                    <br class = 'new-line'></br>
+                    <t className = 'work-description'>
+                    Lorem ipsum dolor sit amet, scelerisque vestibulum, faucibus egestas vehicula mi viverra auctor tristique. Pede montes, rhoncus tempus a per, ante et. Et bibendum ridiculus illo praesent in iure, sed ut posuere. Dui a dignissimos aliquam hendrerit consectetuer ipsum, malesuada sit morbi, iaculis non.</t>
+                    <br></br>
+                    <div className = 'skills'>
+                        <div className = 'language'>
+                            <b className = 'language-body'>Python</b>
+                        </div>
+                        <div className = 'technology'>
+                            <b className = 'technology-body'>Node.js</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>JavaScript</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>Python</b>
+                        </div>
+                        <div className = 'technology'>
+                            <b className = 'technology-body'>Node.js</b>
+                        </div>
+                        <div className = 'language'>
+                            <b className = 'language-body'>JavaScript</b>
+                        </div>
+                    </div>
+                </div>
+                
+               
+               
+                
             </div> 
 
 
